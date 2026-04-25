e11b808669dd592191a97e16db60075d0f672f6b
censu <censu@WIN-PLE19FD7KB6>
2026-04-26 00:37:21 +0200

    censured

---
+++ Models/Context/SchedaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Context
+{
+    public class SchedaDbContext : BaseContext
+    {
+        public DbSet<Scheda> Schede { get; set; } = null!;
+        public DbSet<Person> People { get; set; } = null!;
+        public DbSet<Socio> Soci { get; set; } = null!;
+        public DbSet<Tessera> Tessere { get; set; } = null!;
+    }
+}
---
+++ ViewModels/Cassa/Back/ListaSociViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class ListaSociViewModel : BaseViewModel
+    {
+
+        public ListaSociViewModel(IScreen host) : base(host)
+        {
+
+        }
+
+        protected override Task OnEsc()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnLoading()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnSaving()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
